30ca2e1d0f0d936b102aef04d7a037bb26572a13
Nesterenko <vith@yandex.ru>
2022-11-28 11:48:47 +0500

    added table

--- 6-sort/compareTable.js
+++ 6-sort/compareTable.js
@@ -4,9 +4,6 @@ import {shellSort, shellSortKnuth} from "./shell.js";
 import {compTable} from "./util.js";
 
 
-
-
-
 compTable(bubbleSort, "Bubble sort", 9000);
 compTable(insertionSort, "insertionSort", 900000);
 compTable(shellSort, "shellSort");
--- 6-sort/util.js
+++ 6-sort/util.js
@@ -13,6 +13,35 @@ export const getRandomArray = n => {
   return a;
 }
 
+const getRandomDigitsArray = n => {
+  let a = [];
+
+  for (let i = 0; i < n; i++) {
+    a.push(getRandomInt(0, 9));
+  }
+  return a;
+}
+
+const getAlmostSortedArray = n => {
+  const arr = getRandomArray(n);
+  const swap = (i, j) => {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  for (let i = 0; i < Math.round(n / 100); i++) {
+    let i1 = getRandomInt(0, n - 1);
+    let i2 = getRandomInt(0, n - 1);
+    swap(i1, i2);
+  }
+  return arr;
+}
+
+const getReversedSortedArray = n => {
+  return getRandomArray(n).reverse();
+};
+
 export function measureFn(fn, ...args) {
   var startTime = performance.now()
   fn(...args)
@@ -22,12 +51,64 @@ export function measureFn(fn, ...args) {
 
 export function compTable(fn, description, max = 1000000) {
   const ns = [100, 10000, 100000, 1000000].filter(e => e <= max);
-  console.log(description);
+
+  console.log(description, "random array");
+
+  const t = {
+    // N: n,
+    "random": {},
+    "sorted": {},
+    "digits": {},
+    "reversed": {}
+  };
+
+
   ns.forEach(n => {
-    const t = {N: n, time: measureFn(fn, getRandomArray(n))};
-    console.table(t);
+    t["random"][n] = t["random"][n] || {};
+    t["sorted"][n] = t["sorted"][n] || {};
+    t["digits"][n] = t["digits"][n] || {};
+    t["reversed"][n] = t["reversed"][n] || {};
+    t["random"][n] = measureFn(fn, getRandomArray(n));
+    t["sorted"][n] = measureFn(fn, getAlmostSortedArray(n));
+    t["digits"][n] = measureFn(fn, getRandomDigitsArray(n));
+    t["reversed"][n] = measureFn(fn, getReversedSortedArray(n));
   });
+
+  console.table(t)
+
+
+  // ns.forEach(n => {
+  //   const t = {N: n, time: measureFn(fn, getRandomArray(n))};
+  //   console.table(t);
+  // });
+
+
+  // ns.forEach(n => {
+  //   const t = {N: n, time: measureFn(fn, getRandomArray(n))};
+  //   console.table(t);
+  // });
+
+  // console.log(description, "random digits array");
+  // ns.forEach(n => {
+  //   const t = {N: n, time: measureFn(fn, getRandomDigitsArray(n))};
+  //   console.table(t);
+  // });
+
+  // console.log(description, "random almost sorted array");
+  // ns.forEach(n => {
+  //   const t = {N: n, time: measureFn(fn, getAlmostSortedArray(n))};
+  //   console.table(t);
+  // });
+
+
+  // console.log(description, "random reversed sorted array");
+  // ns.forEach(n => {
+  //   const t = {N: n, time: measureFn(fn, getAlmostSortedArray(n))};
+  //   console.table(t);
+  // });
 
 
 }
+
+
 
